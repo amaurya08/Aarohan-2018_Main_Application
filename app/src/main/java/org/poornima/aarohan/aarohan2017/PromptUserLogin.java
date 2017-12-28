1f3efcd15be174a864942973bf6fa6c3a9767de9
mayank <mayankjn0001@gmail.com>
2017-12-28 12:31:57 +0530

    second commit updated content

--- app/src/main/java/org/poornima/aarohan/aarohan2017/PromptUserLogin.java
+++ app/src/main/java/org/poornima/aarohan/aarohan2017/PromptUserLogin.java
@@ -1,13 +1,29 @@
 package org.poornima.aarohan.aarohan2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PromptUserLogin extends AppCompatActivity {
-
+ private Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompt_user_login);
+        init();
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PromptUserLogin.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void init(){
+        b=findViewById(R.id.login_button);
     }
 }
