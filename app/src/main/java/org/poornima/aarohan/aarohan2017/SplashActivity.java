32d9942e12e03da06e9402eecb1d295313b506ec
kuldeep <itsmekuldeep07@hotmail.com>
2017-12-29 14:12:12 +0530

    Splash Screen wit loder by kuldeep

--- app/src/main/java/org/poornima/aarohan/aarohan2017/SplashActivity.java
+++ app/src/main/java/org/poornima/aarohan/aarohan2017/SplashActivity.java
@@ -4,18 +4,48 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    private  ProgressBar mProgress;
+    TextView splash_prog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
+
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+        splash_prog=(TextView)findViewById(R.id.splash_progress);
+
+
+      new Thread(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,PromptUserLogin.class));
+                doWork();
+                startApp();
+                finish();
             }
-        },3000);
+        }).start();
     }
+
+        private void doWork() {
+            for (int progress=0; progress<100; progress+=1) {
+                try {
+                    Thread.sleep(30);
+                    splash_prog.setText("Loading..."+String.valueOf(progress)+"%");
+                    mProgress.setProgress(progress);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void startApp() {
+            Intent intent = new Intent(SplashActivity.this, PromptUserLogin.class);
+            startActivity(intent);
+        }
+
 }
