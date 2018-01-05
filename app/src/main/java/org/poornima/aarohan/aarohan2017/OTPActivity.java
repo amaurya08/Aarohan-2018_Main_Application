37d58f0c60b84933d57251fb6d76172e3f8b3585
Ashish <amaurya080197@gmail.com>
2018-01-05 10:47:43 +0530

    Login Activity Changed but need to be verified

---
+++ app/src/main/java/org/poornima/aarohan/aarohan2017/OTPActivity.java
@@ -0,0 +1,232 @@
+package org.poornima.aarohan.aarohan2017;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.poornima.aarohan.aarohan2017.AarohanClasses.NetWorkManager;
+import org.poornima.aarohan.aarohan2017.AarohanClasses.URLHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OTPActivity extends AppCompatActivity {
+    private static final String TAG = "DEBUG";
+    private TextView resend;
+    private EditText otp;
+    private String intentEmail;
+    private Button verify;
+    private ProgressDialog progressDialog;
+    private TextView countdownTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+        methodListener();
+        progressDialog.setMessage("Sending OTP....");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        if (NetWorkManager.checkInternetAccess(OTPActivity.this)) {
+            sendOTP(intentEmail);
+        } else
+            Toast.makeText(OTPActivity.this, "No Internet Access", Toast.LENGTH_SHORT).show();
+    }
+
+    private void init() {
+        intentEmail = getIntent().getStringExtra("email");
+        otp = findViewById(R.id.otp);
+        progressDialog = new ProgressDialog(OTPActivity.this);
+        verify = findViewById(R.id.verify);
+        resend = findViewById(R.id.resend);
+        
+        countdownTextView=(TextView)findViewById(R.id.countdownTimer);
+    }
+
+    private void methodListener() {
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (otp.getText().toString().length() == 8) {
+                    progressDialog.setMessage("Verifying OTP....");
+                    progressDialog.setCancelable(false);
+                    if (NetWorkManager.checkInternetAccess(OTPActivity.this)) {
+                        progressDialog.show();
+                        checkOTP(otp.getText().toString());
+                    } else {
+                        Toast.makeText(OTPActivity.this, "No Internet", Toast.LENGTH_SHORT).show();
+                    }
+                } else
+                    Toast.makeText(OTPActivity.this, "Invalid OTP", Toast.LENGTH_SHORT).show();
+            }
+        });
+        resend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resendotp();
+            }
+        });
+    }
+
+    private void resendotp() {
+        resend.setVisibility(View.INVISIBLE);
+        countdownTextView.setVisibility(View.VISIBLE);
+        sendOTP(intentEmail);
+        countdown();
+    }
+
+    private void countdown() {
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                countdownTextView.setText("Retry after: " + millisUntilFinished / 1000 +"seconds");
+            }
+            public void onFinish() {
+                resend.setVisibility(View.VISIBLE);
+                countdownTextView.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+    }
+
+    private void checkOTP(final String text) {
+        try {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URLHelper.verifyOTP, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        progressDialog.cancel();
+                        Log.d(TAG, "Checking OTP");
+                        parseCheckOTPString(response);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.cancel();
+                    Toast.makeText(OTPActivity.this, "Please Check Your Internet Connection", Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("email", intentEmail);
+                    map.put("otp", text);
+                    return map;
+                }
+            };
+            RequestQueue queue = Volley.newRequestQueue(OTPActivity.this);
+            queue.add(stringRequest);
+
+        } catch (Exception e) {
+            Log.d(TAG, "" + e.toString());
+        }
+    }
+
+    private void parseCheckOTPString(String response) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject(response);
+        String error = jsonObject.getString("error");
+        String message = jsonObject.getString("message");
+        if (error.equals("false")) {
+            makeSession(message);
+            Intent intent = new Intent(OTPActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else
+            Toast.makeText(this, "" + message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void makeSession(String message) {
+        SharedPreferences sharedPref = getSharedPreferences("aarohan", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("email", intentEmail);
+        editor.putString("otp", otp.getText().toString());
+        editor.putString("sid", message);
+        editor.putBoolean("is", true);
+        Log.d(TAG, "Making Session with " + intentEmail + " " + otp.getText().toString() + " " + message);
+        editor.apply();
+    }
+
+    private void sendOTP(final String intentEmail) {
+        try {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URLHelper.sendOTP, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        progressDialog.cancel();
+                        Log.d(TAG, "OTP SENT");
+                        parseStringOTP(response);
+                    } catch (Exception e) {
+                        Toast.makeText(OTPActivity.this, "" + e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.cancel();
+                    Toast.makeText(OTPActivity.this, "" + error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("email", intentEmail);
+                    return map;
+                }
+            };
+            stringRequest.setRetryPolicy(
+                    new DefaultRetryPolicy(
+                            10000,
+                            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+                    )
+            );
+            RequestQueue queue = Volley.newRequestQueue(OTPActivity.this);
+            queue.add(stringRequest);
+
+        } catch (Exception e) {
+            Log.d(TAG, "" + e.toString());
+        }
+    }
+
+    private void parseStringOTP(String response) throws JSONException {
+        JSONObject jsonObject = new JSONObject(response);
+        String error = jsonObject.getString("error");
+        String message = jsonObject.getString("message");
+        if (error.equals("false")) {
+            Toast.makeText(this, "" + message, Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, "Email ID is not Registered. Please Contact Web / Aplication Developer", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
--- app/src/main/java/org/poornima/aarohan/aarohan2017/PromptUserLogin.java
+++ app/src/main/java/org/poornima/aarohan/aarohan2017/PromptUserLogin.java
@@ -2,47 +2,19 @@ package org.poornima.aarohan.aarohan2017;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.poornima.aarohan.aarohan2017.AarohanClasses.NetWorkManager;
-import org.poornima.aarohan.aarohan2017.AarohanClasses.URLHelper;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class PromptUserLogin extends AppCompatActivity {
-    private com.google.android.gms.common.SignInButton loginButton;
+    private Button loginButton, register;
     private TextView skipTextView;
-
     private static final int RC_SIGN_IN = 0;
     private static final String TAG = "DEBUG";
     private final static String errorNoInternet = "No Internet Access";
-    private GoogleSignInClient mGoogleSignInClient;
     private ProgressDialog progressDialog;
 
     @Override
@@ -51,29 +23,19 @@ public class PromptUserLogin extends AppCompatActivity {
         setContentView(R.layout.activity_prompt_user_login);
         init(); //will Initialize All The Variables
         methodListener(); //All Click Listeners
-        googleLogOut();
-        revokeAccess();
-        //Creating Google Sign Options And Client
-
 
     }
 
-
-    //All Method Listeners
     private void methodListener() {
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.sign_in_button:
-                        if (NetWorkManager.checkInternetAccess(PromptUserLogin.this)) {
-                            signIn();
-                        } else
-                            Toast.makeText(PromptUserLogin.this, errorNoInternet, Toast.LENGTH_SHORT).show();
-                        break;
-                }
+                startActivity(new Intent(PromptUserLogin.this, LoginActivity.class));
+                finish();
             }
         });
+
         skipTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,150 +44,20 @@ public class PromptUserLogin extends AppCompatActivity {
             }
         });
 
-    }
-
-
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            updateUI(account);
-        } catch (ApiException e) {
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-        }
-    }
-
-    private void updateUI(GoogleSignInAccount account) {
-        if (NetWorkManager.checkInternetAccess(PromptUserLogin.this)) {
-            progressDialog.setMessage("Verifying Account....");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-            checkUser(account);
-        } else
-            Toast.makeText(this, errorNoInternet, Toast.LENGTH_SHORT).show();
-    }
-
-    private void checkUser(GoogleSignInAccount account) {
-        final GoogleSignInAccount acc = account;
-        try {
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URLHelper.verifyEmail, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    parseString(response, acc);
-                    progressDialog.cancel();
-
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    progressDialog.cancel();
-                    Toast.makeText(PromptUserLogin.this, "Please Check Your Internet Connection or Please Select a Google Account", Toast.LENGTH_SHORT).show();
-                }
-            }) {
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    HashMap<String, String> map = new HashMap<>();
-                    map.put("email", acc.getEmail());
-                    return map;
-                }
-            };
-            stringRequest.setRetryPolicy(
-                    new DefaultRetryPolicy(
-                            5000,
-                            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
-                    )
-            );
-
-            RequestQueue queue = Volley.newRequestQueue(PromptUserLogin.this);
-            queue.add(stringRequest);
-
-        } catch (Exception e) {
-            Log.d(TAG, "" + e.toString());
-        }
-
-    }
-
-    private void
-    parseString(String response, GoogleSignInAccount account) {
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            String error = jsonObject.getString("error");
-            String message = jsonObject.getString("message");
-            if (error.equals("false")) {
-                Toast.makeText(this, "Email Verified", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(PromptUserLogin.this, LoginActivity.class);
-                intent.putExtra("email", account.getEmail());
-                startActivity(intent);
-            } else {
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-                mGoogleSignInClient.signOut()
-                        .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(PromptUserLogin.this, "Please Login with Registered Email-ID", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://aarohan.poornima.org"));
+                startActivity(browserIntent);
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        init();
-        googleLogOut();
-        revokeAccess();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        googleLogOut();
-        revokeAccess();
+        });
     }
 
     private void init() {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestProfile()
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         progressDialog = new ProgressDialog(PromptUserLogin.this);
         loginButton = findViewById(R.id.sign_in_button);
         skipTextView = findViewById(R.id.skip_text);
+        register = findViewById(R.id.register_button);
 
-    }
-
-    private void googleLogOut() {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        mGoogleSignInClient.signOut();
-    }
-
-    private void revokeAccess() {
-
-        mGoogleSignInClient.revokeAccess()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                    }
-                });
     }
 }
