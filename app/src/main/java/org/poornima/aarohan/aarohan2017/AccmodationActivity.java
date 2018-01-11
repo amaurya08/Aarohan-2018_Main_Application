1111c2ec5c08f27dbe2bc6a6039f67008ec83773
mayank <mayankjn0001@gmail.com>
2018-01-11 17:03:55 +0530

    Accomodation done commited by mayank

--- app/src/main/java/org/poornima/aarohan/aarohan2017/AccmodationActivity.java
+++ app/src/main/java/org/poornima/aarohan/aarohan2017/AccmodationActivity.java
@@ -1,7 +1,11 @@
 package org.poornima.aarohan.aarohan2017;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccmodationActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class AccmodationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accmodation);
+
+    }
+public void browser(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://aarohan.poornima.org.com"));
+        startActivity(browserIntent);
     }
 }
