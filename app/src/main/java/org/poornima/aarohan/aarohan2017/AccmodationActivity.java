3b17a5efa4f3f87fe78d5d3f29560ee8ffbd98e4
mayank <mayankjn0001@gmail.com>
2018-01-14 04:02:56 +0530

    Merge branch 'master' of D:\androidprojectfiles\AarohanMainApplication\AarohanApplication with conflicts.

--- app/src/main/java/org/poornima/aarohan/aarohan2017/AccmodationActivity.java
+++ app/src/main/java/org/poornima/aarohan/aarohan2017/AccmodationActivity.java
@@ -5,14 +5,24 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 public class AccmodationActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accmodation);
-
+        textView=(TextView)findViewById(R.id.accomodation);
+        Animation anim = new AlphaAnimation(0.62f,1.0f);
+        anim.setDuration(300);
+        anim.setStartOffset(15);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        textView.startAnimation(anim);
     }
     public void browser(View view){
         Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://aarohan.poornima.org"));
